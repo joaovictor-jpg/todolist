ae9ee4983c9d60981396669057d69b9052a3a11f
joao victor <joaovictormdasilva676@gmail.com>
2023-10-10 20:55:10 -0300

    Add Lombok library

--- src/main/java/br/com/joaovictor/todolist/model/entities/UserModel.java
+++ src/main/java/br/com/joaovictor/todolist/model/entities/UserModel.java
@@ -1,54 +1,21 @@
 package br.com.joaovictor.todolist.model.entities;
 
-import java.io.Serializable;
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
 
+import java.io.Serializable;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class UserModel implements Serializable {
 
     private static final long serialVersionUID = 1L;
     private String userName;
     private String name;
     private String password;
-
     private UserModel() {}
-
-    public UserModel(String userName, String name, String password) {
-        this.userName = userName;
-        this.name = name;
-        this.password = password;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserModel userModel = (UserModel) o;
-        return userName.equals(userModel.userName) && name.equals(userModel.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(userName, name);
-    }
-
-    @Override
-    public String toString() {
-        return "UserModel{" +
-                "userName='" + userName + '\'' +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
