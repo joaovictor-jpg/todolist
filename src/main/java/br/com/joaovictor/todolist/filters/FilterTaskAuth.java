0c570edcfc1f008c53140534c6940a9bf3b50a89
joao victor <joaovictormdasilva676@gmail.com>
2023-10-11 22:35:14 -0300

    filter part 2 Final

--- src/main/java/br/com/joaovictor/todolist/filters/FilterTaskAuth.java
+++ src/main/java/br/com/joaovictor/todolist/filters/FilterTaskAuth.java
@@ -1,15 +1,34 @@
 package br.com.joaovictor.todolist.filters;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
+
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("Passou pelo filtro Task");
-        filterChain.doFilter(servletRequest, servletResponse);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // Pegar a autenticação (usuario e senha)
+        String[] authorization = request.getHeader("Authorization").split(" ");
+
+        byte[] authDecode = Base64.getDecoder().decode(authorization[1]);
+
+        String[] authString = new String(authDecode).split(":");
+
+        String username = authString[0];
+        String password = authString[1];
+
+        
+
+
+        // Passando a requsição para frente
+        filterChain.doFilter(request, response);
     }
 }
