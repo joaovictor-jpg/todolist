c7dbee59a07a7b9777880027c1b56b507a5c3aed
joao victor <joaovictormdasilva676@gmail.com>
2023-10-11 21:22:08 -0300

    Created a new Task table

---
+++ src/main/java/br/com/joaovictor/todolist/model/entities/TaskModel.java
@@ -0,0 +1,33 @@
+package br.com.joaovictor.todolist.model.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_task")
+@EqualsAndHashCode
+@ToString
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    @Column(length = 50)
+    private String title;
+    private String description;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID userId;
+
+    private LocalDateTime createdAt;
+}
